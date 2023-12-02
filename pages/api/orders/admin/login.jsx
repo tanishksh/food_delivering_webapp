ebae920f2b3a84f4bac55da022db3cff7622a60a
tanishksh <91403168+tanishksh@users.noreply.github.com>
2023-12-02 16:08:51 +0530

    Update login.jsx

--- pages/api/orders/admin/login.jsx
+++ pages/api/orders/admin/login.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import styles from "../../styles/Login.module.css";
+import styles from '../../../../styles/Login.module.css';
 
 const Login = () => {
   const [username, setUsername] = useState(null);
